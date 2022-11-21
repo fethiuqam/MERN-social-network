05df5ff64c82b3f61a87af78d3a25650f00a2e4b
Abi Ayad, Fethi Bey <fethibeyabiayad@gmail.com>
2022-11-21 18:40:50 -0500

    add post method to create new user

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose'
 import {log} from "debug";
 import app from './app.js';
-import User from './models/user.js'
-
 
 async function main() {
     dotenv.config();
     const PORT = process.env.DEV_PORT;
     try {
         await mongoose.connect(process.env.DEV_MONGODB_URI);
-        const countUsers = await User.find().count();
-        log(countUsers);
-        if(countUsers === 0){
-            const user1 = new User ({email: "fethibey@gmail.com" , pseudo : "fethiuqam"})
-            await user1.save();
-        }
         app.listen(PORT,
             () => log('server running on http://localhost:' + PORT));
     } catch (err) {
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,21 @@ import express from 'express';
 const router = express.Router();
 import User from '../models/user.js'
 
-/* GET users listing. */
-router.get('/', async function (req, res) {
+// get all users
+router.get('/', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
+});
+
+// create user
+router.post('/', async (req, res) => {
+    const newUser = new User ({
+        email : req.body.email,
+        pseudo : req.body.pseudo
+    });
+    const result = await newUser.save();
+    res.status(201).json(result);
+
 });
 
 export default router;
