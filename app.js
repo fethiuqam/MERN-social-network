5e3b40d4151fe4730fb93c2899a66c293f6a1311
Abi Ayad, Fethi Bey <fethibeyabiayad@gmail.com>
2022-11-21 13:08:50 -0500

    express project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+const {log} = require("debug");
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/users', usersRouter);
+
+app.listen(8080, () => log('server running on http://localhost:8080'));
